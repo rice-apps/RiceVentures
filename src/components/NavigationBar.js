20159ecbb44cd1ade3f6d410abce99cf4f1bb076
Will Mundy <TheWillMundy@users.noreply.github.com>
2020-11-08 20:42:24 -0600

    Added navigation bar and routing

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NavigationBar = () => {
+    return (
+        <h1>NavigationBar</h1>
+    )
+}
+
+export default NavigationBar;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import Routes from "./Routes";
 
 ReactDOM.render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<Routes />
+			<App />
 		</BrowserRouter>
 	</React.StrictMode>,
 	document.getElementById("root")
