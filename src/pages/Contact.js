c3a586f41858b5a1255e1a1aee5ab8d3b7faec86
anthonyy3995 <anthonyy3995@gmail.com>
2020-11-18 10:19:50 -0600

    Updated Contact Page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,46 @@
 import React from "react";
+import "./Contact.css";
 
 const Contact = () => {
     return (
-        <h1>Contact</h1>
+        <div>
+            <div className = "titleContainer">
+                <h1 className = "title">
+                    CONTACT US
+                </h1>
+            </div>
+            <div className = "bottomContainer">
+                <div className = "messageContainer">
+                    <h2 className = "messageTitle">
+                        MESSAGE
+                    </h2>
+                    <input className = "smallInput" />
+                    <input className = "smallInput" style={{top:"472px"}}/>
+                    <input className = "largeInput" />
+                    {/* send button?? */}
+                </div>
+                <div className = "followContainer">
+                    <h2 className = "followTitle">
+                        FOLLOW US
+                    </h2>
+                    <div className = "logoContainer">
+                        <button className = "firstButton" >FB</button>
+                        <button className = "secondButton" >IG</button>
+                        <button className = "thirdButton">SC</button>
+                    </div>
+                    <h2 className = "newsletterTitle">
+                        NEWSLETTER
+                    </h2>
+                    <div className = "newsletter"></div>
+                    
+                    
+                    
+                </div>
+
+            </div>
+        </div>
+        
+        
     )
 }
 
