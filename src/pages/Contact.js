d3b18977315170cee270f08689b6123bfad5563a
Will Mundy <TheWillMundy@users.noreply.github.com>
2021-01-02 00:19:59 -0500

    Updated Contact page with wireframe css and tailwindcss

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,55 @@
 import * as React from "react";
+import "../styles/Contact.css";
 
 // markup
 const ContactPage = () => {
 	return (
 		<main>
 			<title>Rice Ventures</title>
+			<div className="bg-red-100 h-screen flex flex-col text-center">
+				<h1 className="font-medium text-5xl my-10">CONTACT US</h1>
+				<div className="flex flex-row justify-evenly">
+					<div className="w-1/4 flex flex-col items-center text-center">
+						<h2 className="font-medium text-3xl">MESSAGE</h2>
+						<form>
+							<input className="h-10" placeholder="Name" />
+							<input className="h-10" placeholder="Email" />
+							<textarea className="h-40 mb-0" placeholder="Message" />
+							<button
+								className="submitButton"
+								type="submit"
+								onClick={(e) => {
+									e.preventDefault();
+									console.log("Clicked Send!");
+								}}
+							>
+								Send
+							</button>
+						</form>
+					</div>
+					<div className="w-1/4 flex flex-col items-center text-center">
+						<h2 className="font-medium text-2xl">FOLLOW US</h2>
+						<div className="w-1/2 flex flex-row justify-between">
+							<button className="socialMediaButton">
+								FB
+							</button>
+							<button className="socialMediaButton">
+								IG
+							</button>
+							<button className="socialMediaButton">
+								SC
+							</button>
+						</div>
+						<h2 className="font-normal text-2xl m-5">
+							NEWSLETTER
+						</h2>
+						<form>
+							<input className="h-10" placeholder="Email" />
+							<button className="submitButton" type="submit">Send</button>
+						</form>
+					</div>
+				</div>
+			</div>
 		</main>
 	);
 };
