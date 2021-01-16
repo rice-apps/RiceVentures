aeca216ef4911c074c7cb0c9dbdaaedaa929c4d7
Will Mundy <TheWillMundy@users.noreply.github.com>
2021-01-16 12:05:54 -0500

    Navbar added to Layout

---
+++ src/components/Navbar.js
@@ -0,0 +1,44 @@
+import { Link } from "gatsby";
+import * as React from "react";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa";
+import { isBrowser, isMobile } from "react-device-detect";
+
+const pages = [
+	{ title: "Home", link: "/" },
+	{ title: "Meet", link: "/meet" },
+	{ title: "Join", link: "/join" },
+	{ title: "Contact Us", link: "/contact" },
+];
+
+const Navbar = () => {
+	const [open, setOpen] = React.useState(false);
+
+	return (
+		<div className="container mx-auto flex flex-col items-center xl:flex-row uppercase pt-5">
+			<p className="text-xl flex-grow">Rice Ventures</p>
+			<div className="flex flex-col items-center xl:flex-row">
+				{(isMobile && open) || isBrowser ? (
+					<>
+						{pages.map((page) => (
+							<Link
+								className="text-md xl:text-xl px-5"
+								to={page.link}
+							>
+								{page.title}
+							</Link>
+						))}
+						{isMobile ? (
+							<FaChevronUp onClick={() => setOpen(!open)} />
+						) : null}
+					</>
+				) : (
+					<>
+						<FaChevronDown onClick={() => setOpen(!open)} />
+					</>
+				)}
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,9 +1,11 @@
 import * as React from "react";
 import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 const Layout = ({ children }) => {
 	return (
 		<div className="flex flex-col h-screen justify-between">
+			<Navbar />
 			{children}
 			<Footer />
 		</div>
