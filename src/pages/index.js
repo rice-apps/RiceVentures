2e3cd15f7f4edf6d21cc32137dc22da381d95ec4
Will Mundy <TheWillMundy@users.noreply.github.com>
2021-01-18 20:43:11 -0700

    Fixed layout issues on main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,60 +25,60 @@ const FrontPage = ({ data }) => {
 		<main>
 			<title>Rice Ventures</title>
 			<Layout>
-				<div className="container mx-auto flex flex-col xl:grid xl:grid-cols-2 xl:justify-items-center items-center px-5">
-					<div className="flex flex-col items-center xl:items-start text-center xl:text-left">
-						<p className="font-black text-3xl xl:text-6xl">
+				<div className="container mx-auto flex flex-col lg:grid lg:grid-cols-2 lg:justify-items-center items-center px-5">
+					<div className="flex flex-col items-center lg:items-start text-center lg:text-left">
+						<p className="font-black text-3xl lg:text-6xl">
 							{mission}{" "}
-							<span className="font-medium text-2xl xl:text-5xl">
+							<span className="font-medium text-2xl lg:text-5xl">
 								at Rice University
 							</span>
 						</p>
 						<p className="font-light text-xl py-5">
 							{missionSubtext}
 						</p>
-						<div className="w-full xl:w-3/4 flex flex-row justify-center xl:justify-start uppercase">
+						<div className="w-full lg:w-3/4 flex flex-row justify-center lg:justify-start uppercase">
 							<Link className="w-1/3 mr-10" to="/meet">
-								<button className="py-1 px-3 xl:p-3 bg-navy text-white h-full w-full">
+								<button className="py-1 px-3 lg:p-3 bg-navy text-white h-full w-full">
 									Meet the Team
 								</button>
 							</Link>
 							<Link className="w-1/3" to="/join">
-								<button className="py-1 px-3 xl:p-3 bg-white h-full w-full">
+								<button className="py-1 px-3 lg:p-3 bg-white h-full w-full">
 									Join
 								</button>
 							</Link>
 						</div>
 					</div>
-					<div className="w-full xl:w-7/12">
+					<div className="w-full lg:w-7/12">
 						<Img fluid={missionImage.fluid} />
 					</div>
 				</div>
 				<div className="w-screen">
 					<Img fluid={data.curvedImage.childImageSharp.fluid} />
 				</div>
-				<div className="bg-navy text-white pb-10">
+				<div className="bg-navy text-white pb-10 px-5">
 					<div className="container mx-auto">
 						{infoSections.map((section) => {
 							return (
 								<div
 									key={section.order}
-									className="flex flex-col xl:grid xl:grid-flow-col xl:grid-cols-2 xl:justify-items-center"
+									className="flex flex-col lg:grid lg:grid-flow-col lg:grid-cols-2 lg:justify-items-center"
 								>
 									<div
-										className={`flex flex-col items-center xl:items-start justify-center text-center xl:text-left ${
+										className={`flex flex-col items-center lg:items-start justify-center text-center lg:text-left ${
 											section.order % 2
 												? ""
-												: "xl:col-end-reverse"
-										} p-5 xl:px-0`}
+												: "lg:col-end-reverse"
+										} p-5 lg:px-0`}
 									>
-										<p className="font-extrabold text-3xl xl:text-6xl">
+										<p className="font-extrabold text-3xl lg:text-6xl">
 											{section.title}{" "}
 										</p>
 										<div className="font-light text-lg py-5">
 											{section.renderedContent}
 										</div>
 									</div>
-									<div className="w-full xl:w-9/12">
+									<div className="w-full lg:w-9/12">
 										<Img fluid={section.image.fluid} />
 									</div>
 								</div>
